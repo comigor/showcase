223fe51ffa135a16ff4c442fc040f70a2021d113
Igor Borges <igor@borges.me>
2018-10-06 13:44:50 -0300

    add showcase "test" to group

--- test/components/my_widget_test.dart
+++ test/components/my_widget_test.dart
@@ -1,27 +1,29 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:showcase/components/my_widget.dart';
 import '../helpers/showcase.dart';
 
 void main() {
-  final widgetList = [
-    MyWidget(),
-    MyWidget(anotherBuild: true),
-    MyWidget(sliderValue: 5.0),
-  ];
+  group('Showcase widgets', () {
+    final widgetList = [
+      MyWidget(),
+      MyWidget(anotherBuild: true),
+      MyWidget(sliderValue: 5.0),
+    ];
 
-  // showcaseWidgets(widgetList);
-  showcaseWidgets(widgetList, customContainerBuilder: (child) => Container(
-      padding: EdgeInsets.all(12.0),
-      decoration: BoxDecoration(
-        color: Colors.amberAccent,
-        border: Border.all(
-          color: Colors.amber,
-          width: 2.0,
+    showcaseWidgets(widgetList, customContainerBuilder: (child) => Container(
+        padding: EdgeInsets.all(12.0),
+        decoration: BoxDecoration(
+          color: Colors.amberAccent,
+          border: Border.all(
+            color: Colors.amber,
+            width: 2.0,
+          ),
         ),
+        width: 800.0,
+        height: 240.0,
+        child: child,
       ),
-      width: 800.0,
-      height: 240.0,
-      child: child,
-    ),
-  );
+    );
+  });
 }
