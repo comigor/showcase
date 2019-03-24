c04703c729538c9ab4ff04e501dec6d9b759eb04
Igor Borges <igor@borges.me>
2019-03-23 22:44:33 -0300

    Update example

--- example/test/my_widget.showcased_test.dart
+++ example/test/my_widget.showcased_test.dart
@@ -1,10 +1,14 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:showcase/showcase.dart';
 import 'package:showcase_example/my_widget.dart';
 
-void main() {
+Future<void> main() async {
+  final FontLoader fontLoader = FontLoader('Roboto')..addFont(fetchFont());
+  await fontLoader.load();
+
   group('Showcase MyWidget', () {
     showcaseWidgets([MyWidget()]);
   });
