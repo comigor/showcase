f0aa5bbd63a86539dfc12c920aa447ffff16fc81
Igor Borges <igor@borges.me>
2019-03-26 14:54:40 -0300

    Load custom fonts
    Load custom fonts (defined in pubspec.yaml) aside from Roboto before showcasing.

--- lib/fetch_font.dart
+++ lib/fetch_font.dart
@@ -2,24 +2,29 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:yaml/yaml.dart';
 import 'package:http/http.dart' as http;
 import 'package:glob/glob.dart';
 
-Future<ByteData> _fetchFontFromCache() async {
+Future<ByteData> _fetchFontFromFile(File file) async {
+  final Uint8List bytes = Uint8List.fromList(await file.readAsBytes());
+  return ByteData.view(bytes.buffer);
+}
+
+Future<ByteData> _fetchRobotoFromCache() async {
   final String root = Platform.environment['PUB_CACHE'] ??
       '${Platform.environment['HOME']}/.pub-cache';
   await for (FileSystemEntity path in Glob(
           'hosted/pub.dartlang.org/showcase-*/assets/Roboto/Roboto-Regular.ttf')
       .list(root: root)) {
     if (path is File) {
-      final Uint8List bytes = Uint8List.fromList(await path.readAsBytes());
-      return ByteData.view(bytes.buffer);
+      return _fetchFontFromFile(path);
     }
   }
   throw Exception('Failed to load font');
 }
 
-Future<ByteData> _fetchFontFromInternet() async {
+Future<ByteData> _fetchRobotoFromInternet() async {
   final http.Response response = await http.get(
       'https://raw.githubusercontent.com/google/fonts/master/apache/roboto/Roboto-Regular.ttf');
 
@@ -30,9 +35,35 @@ Future<ByteData> _fetchFontFromInternet() async {
   }
 }
 
-/// Fetch Roboto font from local cache, or from the internet, if it's not
-/// found in the cache.
+Future<ByteData> _fetchRoboto() =>
+    _fetchRobotoFromCache().catchError((_) => _fetchRobotoFromInternet());
+
+Future<void> _loadRoboto() async {
+  final FontLoader fontLoader = FontLoader('Roboto')..addFont(_fetchRoboto());
+  await fontLoader.load();
+}
+
+/// Load all fonts found in pubspec.yaml, and fetch Roboto font from local
+/// cache, or from the internet, if it's not found in the cache.
 /// It needs to be done because flutter_test blocks access to package assets
 /// (see https://github.com/flutter/flutter/issues/12999).
-Future<ByteData> fetchFont() =>
-    _fetchFontFromCache().catchError((_) => _fetchFontFromInternet());
+Future<void> loadFonts() async {
+  await _loadRoboto();
+
+  final dynamic yaml = loadYaml(await File('../pubspec.yaml').readAsString());
+  if (yaml is YamlMap) {
+    final dynamic fonts = yaml['flutter']['fonts'];
+    if (fonts is YamlList) {
+      for (final dynamic font in fonts.toList()) {
+        if (font is YamlMap) {
+          final FontLoader loader = FontLoader(font['family'].toString());
+          for (YamlMap configuration in font['fonts']) {
+            loader.addFont(
+                _fetchFontFromFile(File('../${configuration['asset']}')));
+          }
+          await loader.load();
+        }
+      }
+    }
+  }
+}
