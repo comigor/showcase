0a114c6037d4efd973940f8e1fa6719a102a7567
Igor Borges <igor@borges.me>
2019-03-26 16:41:09 -0300

    Example with forDesignTime

---
+++ example/test/my_widget_with_required_props.showcased_test.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:showcase/showcase.dart';
+import 'package:showcase_example/my_widget_with_required_props.dart';
+
+Future<void> main() async {
+  await loadFonts();
+
+  group('Showcase MyWidgetWithRequiredProps', () {
+    showcaseWidgets([MyWidgetWithRequiredProps.forDesignTime()]);
+  });
+}
