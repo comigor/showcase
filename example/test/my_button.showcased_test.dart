6b9466d377c4e982ce8417d7bff05eae9008d088
Igor Borges <igor@borges.me>
2019-03-26 14:55:38 -0300

    Custom font example
    Add a new widget which uses a custom font and showcase it

---
+++ example/test/my_button.showcased_test.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:showcase/showcase.dart';
+import 'package:showcase_example/my_button.dart';
+
+Future<void> main() async {
+  await loadFonts();
+
+  group('Showcase MyButton', () {
+    showcaseWidgets([MyButton()]);
+  });
+}
